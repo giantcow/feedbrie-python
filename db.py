382cc0445e3a2b23f63c1013c187740d8f2db9db
Kayo <kayo@kayo.moe>
2019-05-26 11:44:29 -0700

    more formatting and finishing up

--- db.py
+++ db.py
@@ -12,23 +12,30 @@ log.addHandler(epicfilehandler)
 mariadb_connection = mariadb.connect(host="localhost", user='brie', password='3th3rn3t', db='Brie', autocommit=True)
 cursor = mariadb_connection.cursor()
 
-class DatabaseError(Exception):
+class DatabaseException(Exception):
     def __init__(self, message="This is a generic database error."):
         self.message = message
 
-class InvalidFieldError(Exception):
+class InvalidFieldException(Exception):
     def __init__(self, field, table="users"):
         self.message = f"{field} is not a valid column in the {table} table!"
 
-class NonBooleanType(Exception):
+class NonBooleanTypeException(Exception):
     def __init__(self, message="The passed variable must be of type boolean!"):
         self.message = message
 
-class InvaludUserIdType(Exception):
-    def __init__(self, user_id):
-        self.message = f"{user_id} is not a valid user id!"
+class InvaludUserIdTypeException(Exception):
+    def __init__(self, user_id, reason):
+        self.message = f"{user_id} is not a valid user id because: {reason}"
 
 class Database():
+
+    def user_id_check(user_id):
+    if isinstance(user_id, str):
+        if len(user_id) > 100:
+            raise InvaludUserIdTypeException(user_id=user_id, reason="Max ID length is 100 characters")
+    else:
+        raise InvaludUserIdTypeException(user_id=user_id, reason="Non-string type.")
 
     def __get_table_fields(table):
         fields = []
@@ -41,13 +48,14 @@ class Database():
         except mariadb.Error as error:
             log.error("Failed to get table columns: %s", error)
 
-    __user_fields = __get_table_fields("users")
+    __user_table_fields = __get_table_fields("users")
 
     async def create_new_user(user_id, username):
         '''
         Creates new user entry with default values from config.
         '''
         try:
+            user_id_check(user_id)
             now = time.time()
             now = dt.datetime.fromtimestamp(now).strftime("%Y-%m-%d %H:%M:%S")
 
@@ -56,68 +64,65 @@ class Database():
                 "INSERT INTO users (username,user_id,affection,bond_level,bonds_available,has_feather,has_brush,has_scratcher,free_feed,created_at,updated_at) VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)", 
                 (username,user_id,0,0,0,0,0,0,1,now,now)
             )
-        except mariadb.Error as error:
+        except (mariadb.Error, InvaludUserIdTypeException) as error:
             log.error("Failed to create new user: %s", error)
+
+    async def set_value(index, val_name, val):
+        if val_name not in __user_table_fields: raise InvalidFieldException(field=val_name)
+
+        try:
+            user_id_check(index)
+            cursor.execute("UPDATE users SET %s = %s WHERE user_id = %s", (val_name, val, index))
+        except (mariadb.Error, InvaludUserIdTypeException) as error:
+            log.error("Failed to set %s to %s for user_id: %s \n %s" % (val_name, val, index, error))
+    
+    async def add_value(index, val_name, val):
+        if val_name not in __user_table_fields: raise InvalidFieldException(field=val_name)
+
+        try:
+            user_id_check(index)
+            cursor.execute("UPDATE users SET %s = %s + %s WHERE user_id = %s", (val_name, val_name, val, index))
+        except (mariadb.Error, InvaludUserIdTypeException) as error:
+            log.error("Failed to set %s to %s for user_id: %s \n %s" % (val_name, val, index, error))
+
+    async def remove_value(index, val_name, val):
+        if val_name not in __user_table_fields: raise InvalidFieldException(field=val_name)
+
+        try:
+            user_id_check(index)
+            cursor.execute("UPDATE users SET %s = %s - %s WHERE user_id = %s", (val_name, val_name, val, index))
+        except (mariadb.Error, InvaludUserIdTypeException) as error:
+            log.error("Failed to set %s to %s for user_id: %s \n %s" % (val_name, val, index, error))
+
+    async def get_value(index, val_name):
+        if val_name not in __user_table_fields: raise InvalidFieldException(field=val_name)
+
+        try:
+            user_id_check(index)
+            cursor.execute("SELECT %s FROM users WHERE user_id = %s", (index, val_name))
+            res = cursor.fetchall()
+            return res[0][0]
+        except (mariadb.Error, InvaludUserIdTypeException) as error:
+            log.error("Failed to get %s for user_id: %s \n %s" % (val_name, index, error))
+
+    #########################
+    #                       #
+    #       Helpers         #
+    #                       #
+    #########################
 
     async def get_created_timestamp(user_id):
         '''
         Returns timestamp for when the user entry was created.
         '''
-        try:
-            cursor.execute("SELECT created_at FROM users WHERE user_id = %s", [user_id])
-            res = cursor.fetchall()
-            return res[0][0]
-        except mariadb.Error as error:
-            log.error("Failed to get created_at timestamp for user_id: %s \n %s" % (user_id, error))
+        return get_value(user_id, "created_at")        
 
     async def get_updated_timestamp(user_id):
         '''
         Returns the last time the given User's entry was updated.
         '''
-        try:
-            cursor.execute("SELECT updated_at FROM users WHERE user_id = %s", [user_id])
-            res = cursor.fetchall()
-            return res[0][0]
-        except mariadb.Error as error:
-            log.error("Failed to get updated_at timestamp for user_id: %s \n %s" % (user_id, error))
+        return get_value(user_id, "updated_at")
 
-    async def set_value(index, val_name, val):
-        if val_name not in __user_fields: raise InvalidFieldError(field=val_name)
-
-        try:
-            cursor.execute("UPDATE users SET %s = %s WHERE user_id = %s", (val_name, val, index))
-        except mariadb.Error as error:
-            log.error("Failed to set %s to %s for user_id: %s \n %s" % (val_name, val, index, error))
-    
-    async def add_value(index, val_name, val):
-        if val_name not in __user_fields: raise InvalidFieldError(field=val_name)
-
-        try:
-            cursor.execute("UPDATE users SET %s = %s + %s WHERE user_id = %s", (val_name, val_name, val, index))
-        except mariadb.Error as error:
-            log.error("Failed to set %s to %s for user_id: %s \n %s" % (val_name, val, index, error))
-
-    async def remove_value(index, val_name, val):
-        if val_name not in __user_fields: raise InvalidFieldError(field=val_name)
-
-        try:
-            cursor.execute("UPDATE users SET %s = %s - %s WHERE user_id = %s", (val_name, val_name, val, index))
-        except mariadb.Error as error:
-            log.error("Failed to set %s to %s for user_id: %s \n %s" % (val_name, val, index, error))
-
-    async def get_value(index, val_name):
-        if val_name not in __user_fields: raise InvalidFieldError(field=val_name)
-
-        try:
-            cursor.execute("SELECT %s FROM users WHERE user_id = %s", (index, val_name))
-            res = cursor.fetchall()
-            return res[0][0]
-        except mariadb.Error as error:
-            log.error("Failed to get %s for user_id: %s \n %s" % (val_name, index, error))
-
-    #################
-    #   Feeding     #
-    #################
     async def set_fed_brie_timestamp(user_id, fed_timestamp=time.time().dt.datetime.fromtimestamp(now).strftime("%Y-%m-%d %H:%M:%S")):
         '''
         Updates the last time a user has fed Brie.
