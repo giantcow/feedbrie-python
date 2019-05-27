3a0d0552c9ae6ae321e185d5b4227fb2167e100e
JayLaDenpa <jrsylightning@gmail.com>
2019-05-27 02:43:51 -0400

    initial storefront handler code

--- storefront.py
+++ storefront.py
@@ -1,8 +1,43 @@
 import json
+import logging
+import random
+from db import Database as db
 
-class StoreFront:
+log = logging.getLogger("storefront")
+epicfilehandler = logging.FileHandler("store.log")
+epicfilehandler.setFormatter(logging.Formatter("%(asctime)s %(levelname)s %(message)s"))
+log.setLevel(logging.DEBUG)
+log.addHandler(epicfilehandler)
+
+class NotEnoughSPError(Exception):
     def __init__(self):
-        with open('store.json') as inv_file:
-            self.store = json.load(inv_file)
+        self.message = "Not enough SP to purchase item."
+
+class NoItemError(Exception):
+    def __init__(self, item):
+        self.message = f"{item} is not a store item."
+
+class StoreLoader:
+    @staticmethod
+    def load_store(path="store.json"):
+        '''
+        Load store from a JSON file
+        '''
+        data = {}
+        try:
+            with open(path) as f:
+                data = json.load(f)
+        except:
+            log.exception("Failed to load Bonds JSON.")
+        return data
+
+class StoreHandler:
+    store_list = StoreLoader.load_store()
     
-    
+    @staticmethod
+    def reload_store(path="store.json"):
+        '''
+        Reload store from disk
+        '''
+        StoreHandler.store_list = StoreLoader.load_store(path)
+        log.info("Reloading store from JSON.")
